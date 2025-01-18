809bbeef5aec177071f466933ab22a2d32412bf0
Deril Jose <deril2004@gmail.com>
2025-01-18 11:52:52 +0530

    Minor design changes

---
+++ base/portrait_base.js
@@ -0,0 +1,53 @@
+async function addPortriatNav() {
+    try {
+        const resp = await fetch("../base/portrait_base.html");
+        if (!resp.ok) {
+            throw new Error(`HTTP error! Status: ${resp.status}`);
+        }
+        const html = await resp.text();
+        document.body.insertAdjacentHTML("afterbegin", html);
+
+        adjustNavigationVisibility();
+    } catch (error) {
+        console.error("Error fetching or inserting footer:", error);
+    }
+}
+
+function adjustNavigationVisibility() {
+    const navigation = document.getElementById("navigation");
+    const portraitNav = document.getElementById("portraitNav");
+
+    if (portraitNav) {
+        if (window.matchMedia("(max-width: 768px)").matches) {
+            navigation.style.display = "none";
+            portraitNav.style.display = "flex";
+        } else {
+            portraitNav.style.display = "none";
+            navigation.style.display = "flex";
+        }
+    } else {
+        console.warn("portraitNav element not found.");
+    }
+}
+
+function selectNav() {
+    document.addEventListener("click", (event) => {
+        const navButton = event.target.closest("#navButton");
+        if (navButton) {
+            const nav = document.querySelector(".nav");
+            nav.classList.add("navOpen");
+        }
+
+        const navOverlay = event.target.closest(".navOverlay");
+        if (navOverlay) {
+            const nav = document.querySelector(".nav");
+            nav.classList.remove("navOpen");
+        }
+    });
+}
+
+addPortriatNav();
+
+window.addEventListener("resize", adjustNavigationVisibility);
+
+document.addEventListener("DOMContentLoaded", selectNav);
---
+++ project/project.js
@@ -0,0 +1,132 @@
+function project(
+    containerID,
+    projectName,
+    projectDesc,
+    libListID,
+    libraries,
+    githubButtonClass,
+    link,
+    aniDelay
+) {
+    const container = document.createElement("div");
+    container.setAttribute("id", containerID);
+    container.setAttribute("class", "project_tiles");
+    document.getElementsByClassName("content")[0].appendChild(container);
+
+    const name = document.createElement("h2");
+    name.innerHTML = projectName;
+    container.appendChild(name);
+
+    const desc = document.createElement("p");
+    desc.innerHTML = projectDesc;
+    container.appendChild(desc);
+
+    const lib = document.createElement("h3");
+    lib.innerHTML = "Tools Utilized:";
+    container.appendChild(lib);
+
+    const libList = document.createElement("ul");
+    libList.setAttribute("class", libListID);
+    container.appendChild(libList);
+
+    libraries.forEach((library) => {
+        const libItem = document.createElement("li");
+        libItem.innerHTML = library;
+        libList.appendChild(libItem);
+    });
+
+    const linkButton = document.createElement("button");
+    linkButton.setAttribute("class", githubButtonClass);
+    linkButton.setAttribute(
+        "onclick",
+        "window.open(" + JSON.stringify(link) + ")"
+    );
+    container.appendChild(linkButton);
+
+    let githubLogo = document.createElement("img");
+    githubLogo.setAttribute("src", "github_logo.png");
+    linkButton.appendChild(githubLogo);
+
+    container.style.animationDelay = aniDelay;
+}
+
+project(
+    "project1",
+    "Portfolio Website",
+    "My Portfolio Website made with HTML ,CSS and Javascript",
+    "libUsed-1",
+    ["HTML", "CSS", "Javascript"],
+    "githubButton-1",
+    "https://github.com/deriljose/portfolio-website-1",
+    "1.25s"
+);
+
+project(
+    "project2",
+    "Data Analysis with Python (FreeCodeCamp)",
+    "All the projects I did for the Data Analysis with Python course",
+    "libUsed-2",
+    ["Python", "NumPy", "Pandas", "Seaborn", "Matplotlib"],
+    "githubButton-2",
+    "https://github.com/deriljose/data_analysis_projects",
+    "1.75s"
+);
+
+const projectTiles = document.querySelectorAll(".project_tiles");
+
+projectTiles.forEach((projectTile) => {
+    projectTile.addEventListener("mousemove", (e) => {
+        const { left, top, width, height } =
+            projectTile.getBoundingClientRect();
+        const mouseX = e.clientX - left;
+        const mouseY = e.clientY - top;
+
+        const centerX = width / 2;
+        const centerY = height / 2;
+
+        const tiltY = (mouseX - centerX) / centerX;
+
+        let tiltX = (mouseY - centerY) / centerY;
+
+        if (tiltX < 0) {
+            tiltX = tiltX * -1;
+        }
+
+        projectTile.style.transform = `rotateX(${tiltX * 7}deg) rotateY(${
+            tiltY * 7
+        }deg)`;
+    });
+
+    projectTile.addEventListener("mouseleave", () => {
+        projectTile.style.transform = "rotateX(0deg) rotateY(0deg)";
+    });
+});
+if (window.innerWidth >= 768) {
+    const projectTiles2 = document.querySelectorAll(".project_tiles");
+
+    projectTiles2.forEach((tile, index) => {
+        if (index > 1) {
+            if (index % 2 == 0) {
+                const previousTile2 = projectTiles2[index - 2];
+                const previousTile = projectTiles2[index - 1];
+                const heightDifference = Math.abs(
+                    previousTile2.offsetHeight - previousTile.offsetHeight
+                );
+
+                if (previousTile.offsetHeight > previousTile2.offsetHeight) {
+                    tile.style.marginTop = `${heightDifference * -1}px`;
+                }
+            } else {
+                const previousTile2 = projectTiles2[index - 3];
+                const previousTile = projectTiles2[index - 2];
+                const heightDifference = Math.abs(
+                    previousTile2.offsetHeight - previousTile.offsetHeight
+                );
+
+                if (previousTile.offsetHeight < previousTile2.offsetHeight) {
+                    tile.style.marginTop = `${heightDifference * -1}px`;
+                }
+            }
+        }
+    });
+}
